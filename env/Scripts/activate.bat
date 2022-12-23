86cb9bd46a25482fa99b0da98a3a8337f4b9ed25
TranNhatMinh23 <Trannhatminh23082001@gmail.com>
2022-12-23 19:04:14 +0700

    Jenkins CI/CICD

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Django\Sourececode\appqiuz\profitlo\portifolio\env
+set VIRTUAL_ENV=E:\Django\profitlo\portifolio\env
 
 if not defined PROMPT set PROMPT=$P$G
 
